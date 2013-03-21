e2b1717fdf1263c8e476edaaf48dd21c20bc6f64
Tomas Calleja <pepetox@gmail.com>
2013-03-21 13:37:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catorceava::Application.config.secret_token = 'dd95933d0323bd27daef772807077a92746b92f66ecad1c6a508c8f984b9a8e86f892fd9f18139d940d07126f2bb3dcea8e5568adc67a9f9eaa6882f85f4ad28'
